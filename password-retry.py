c99c1e8a88fd06f094fe232540cc287067054759
Rax Chen <rockpose904@gmail.com>
2019-07-27 12:05:47 +0800

    Update New

--- password-retry.py
+++ password-retry.py
@@ -1,18 +1,17 @@
-number = 3
-number1 = 2
+number = 3 #剩餘機會
+password = 'a123456'
 
 while number > 0:
-	password = input('請輸入密碼: ')
-	if password == 'a123456':
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
 		print('登入成功!')
-		break
+		break #逃出迴圈
 
-	elif password != 'a123456':
+	elif pwd != password:
 		
-		if number1 > 0:
-			print ('密碼錯誤! 還有', number1 ,'次機會(最多輸入3次密碼)')
+		if number > 1:
 			number = number - 1
-			number1 = number1 -1
-		elif number1 >= 0:
+			print ('密碼錯誤! 還有', number ,'次機會(最多輸入3次密碼)')
+		elif number == 1:
 			print('密碼錯誤超過3次，已鎖定!')
 			break
