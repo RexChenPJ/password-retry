8589eb1e57156865c2cf9dbb90bd19da6b0ab20c
Rax Chen <rockpose904@gmail.com>
2019-07-27 12:13:11 +0800

    Add Comment

--- password-retry.py
+++ password-retry.py
@@ -1,3 +1,9 @@
+#密碼重式程式
+#讓使用者重複輸入密碼
+#最多輸入錯誤三次
+#如果正確，印出登入成功
+#如果不正確，印出密碼錯誤
+
 number = 3 #剩餘機會
 password = 'a123456'
 
@@ -12,6 +18,6 @@ while number > 0:
 		if number > 1:
 			number = number - 1
 			print ('密碼錯誤! 還有', number ,'次機會(最多輸入3次密碼)')
-		elif number == 1:
+		else:
 			print('密碼錯誤超過3次，已鎖定!')
 			break
